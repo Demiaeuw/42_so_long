38eeaf400203982b359cd35deaa95128d32f8098
CABARBAYE Adrien <cabarbaye.adrien@gmail.com>
2024-04-12 14:54:14 +0200

    Initialisation du projet

---
+++ so_long/include/so_long.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acabarba <acabarba@student.42Perpignan.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/12 14:41:04 by acabarba          #+#    #+#             */
+/*   Updated: 2024/04/12 14:41:04 by acabarba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONH_H
+# define SO_LONG_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <stdarg.h>
+
+int	main(void);
+
+#endif
